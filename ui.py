28a08268ea2a7f7411bca1f74163646c054acc45
Mike Bird <mike@arrendy.com>
2023-11-15 15:35:05 -0500

    added symbol input to ui

--- ui.py
+++ ui.py
@@ -7,11 +7,27 @@ st.title("Ask the SEC")
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 
+# TODO: delete - For testing purposes
+def my_chat_function(input):
+    return f"{input}? You got that right!"
+
+# TODO: delete - For testing purposes
+def set_stock(symbol):
+    print(symbol)
+
+# Ticker input
+st.header("Please enter the stock symbol")
+symbol = st.text_input(label="symbol",key="symbol")
+if symbol:
+    set_stock(symbol)
+
 # Text input for user message
-user_input = st.text_input("Your Message", key="user_input")
+user_input = st.chat_input("Your Message", key="user_input")
+
+
 
 # Button to send message
-if st.button("Send"):
+if user_input:
     # Here, integrate with your backend script
     response = my_chat_function(user_input)  # example function call
     st.session_state.chat_history.append(("You", user_input))
@@ -20,3 +36,5 @@ if st.button("Send"):
 # Display chat history
 for user, message in st.session_state.chat_history:
     st.text(f"{user}: {message}")
+
+
